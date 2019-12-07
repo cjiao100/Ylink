eab0b34663c0ef1806004b5542bc1aad2826a2b7
cjiao100 <1971416012@qq.com>
2019-12-07 18:21:31 +0800

    登录页面

---
+++ src/login/loginStyle.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const login = StyleSheet.create({
+  input: {
+    borderBottomColor: '#963830',
+    borderBottomWidth: 2,
+  },
+});
+
+export default login;
