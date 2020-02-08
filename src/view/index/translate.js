e1bdd859a9a200c96ad06d535ae87daadc86768c
cjiao100 <1971416012@qq.com>
2020-02-08 14:53:42 +0800

    搜索页面

--- src/view/index/translate.js
+++ src/view/index/translate.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { color } from '../../assets/styles/theme';
 
 class Translate extends Component {
   constructor(props) {
@@ -8,11 +9,42 @@ class Translate extends Component {
 
   render() {
     return (
-      <View>
-        <Text>123</Text>
+      <View style={translateStyle.container}>
+        <View style={translateStyle.title}>
+          <Text style={translateStyle.title_text}>英领</Text>
+        </View>
+        <View style={translateStyle.input}>
+          <TextInput style={translateStyle.input_content} />
+        </View>
       </View>
     );
   }
 }
+
+const translateStyle = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  title: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title_text: {
+    textAlign: 'center',
+    fontSize: 50
+  },
+  input: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    padding: 20
+  },
+  input_content: {
+    height: 40,
+    borderColor: color.primary_color,
+    borderRadius: 50,
+    borderWidth: 1.5,
+    padding: 10
+  }
+});
 
 export default Translate;
